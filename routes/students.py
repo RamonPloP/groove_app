d7c4b2901db6b74aa36e459fb94c0307a974f299
RamonPloP <aspireandlive@gmail.com>
2025-03-23 18:40:02 -0600

    add: dates filters in active members

--- routes/students.py
+++ routes/students.py
@@ -5,6 +5,8 @@ from models.memberships import Memberships
 from models.teachers import Teachers
 from models.utils import is_admin
 from models.students import Students
+from datetime import datetime
+from flask import request
 from db import db
 from controllers.studentsController import addEditStudent, deleteStudent, addRegulationPDF
 import logging
@@ -87,19 +89,43 @@ def active_members_view():
 @is_admin
 @login_required
 def active_members():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+
+    if not start_date or not end_date:
+        current_date = datetime.now()
+        start_date = datetime(current_date.year, current_date.month, 1).date()
+        end_date = current_date.date()
+
+    start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+
     memberships = Memberships.get_all()
     info = []
     total_members = 0
     amount_total = 0
+
     for membership in memberships:
-        total_members += db.session.query(Students).filter(Students.membership_id==membership.id).filter_by(status=1).count()
-        amount_total += membership.cost*int(db.session.query(Students).filter(Students.membership_id==membership.id).filter_by(status=1).count())
+        active_students_query = db.session.query(Students).filter(Students.membership_id == membership.id).filter_by(
+            status=1)
+
+        active_students_query = active_students_query.filter(Students.start_date >= start_date,
+                                                             Students.start_date <= end_date)
+
+        active_students_count = active_students_query.count()
+
+        total_members += active_students_count
+        amount_total += membership.cost * active_students_count
+
         info.append({
             'name': membership.name,
-            'actives':  db.session.query(Students).filter(Students.membership_id==membership.id).filter_by(status=1).count(),
-            'total': membership.cost*int(db.session.query(Students).filter(Students.membership_id==membership.id).filter_by(status=1).count())
+            'actives': active_students_count,
+            'total': membership.cost * active_students_count
         })
 
-    data = [info, db.session.query(Students).filter_by(status=0).count(), total_members, amount_total]
+    inactive_students_count = db.session.query(Students).filter_by(status=0).filter(Students.start_date >= start_date,
+                                                                                    Students.start_date <= end_date).count()
+
+    data = [info, inactive_students_count, total_members, amount_total]
 
     return data
--- static/js/students/actives.js
+++ static/js/students/actives.js
@@ -1,85 +1,108 @@
+$(function() {
+  let currentDate = new Date();
+  let firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  let today = currentDate.toISOString().split('T')[0];
 
-    $(function() {
-      get_data('/students/active-members/get')
-    });
+  $('#startDateFilter').val(firstDayOfMonth.toISOString().split('T')[0]);
+  $('#endDateFilter').val(today);
 
-    function get_data(url) {
-        $.getJSON(url, function (data) {
-                update_table(data[0])
-                update_graph(data[0])
-                update_totals(data)
-            }
-        );
+  get_data('/students/active-members/get', firstDayOfMonth.toISOString().split('T')[0], today);
+
+  $('#filterButton').click(function() {
+
+    var startDate = $('#startDateFilter').val();
+    var endDate = $('#endDateFilter').val();
+
+    if (!startDate || !endDate) {
+      alert('Por favor, selecciona ambas fechas.');
+      return;
     }
 
+    if (startDate > endDate) {
+      alert('La fecha de inicio no puede ser posterior a la fecha de fin.');
+      return;
+    }
+
+    get_data('/students/active-members/get', startDate, endDate);
+  });
+});
+
+function get_data(url, startDate = '', endDate = '') {
+  $.getJSON(url, { start_date: startDate, end_date: endDate }, function(data) {
+    update_table(data[0]);
+    update_graph(data[0]);
+    update_totals(data);
+  });
+}
+
 function update_totals(data){
-    $('#inactives').html('Bajas: '+data[1]);
-    $('#total_members').html(data[2]);
-    $('#amount_total').html('Total: '+data[3].toLocaleString("es-MX", { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
+  $('#inactives').html('Bajas: '+data[1]);
+  $('#total_members').html(data[2]);
+  $('#amount_total').html('Total: '+data[3].toLocaleString("es-MX", { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
 }
 
 function update_table(data) {
-    let elements = []
-    $.each(data, function (i, element) {
-        let item = []
-        item.push(element.name)
-        item.push(element.actives)
-        item.push('$ ' + element.total.toLocaleString("es-MX", { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
-        elements.push(item)
-    })
+  let elements = []
+  $.each(data, function(i, element) {
+    let item = []
+    item.push(element.name);
+    item.push(element.actives);
+    item.push('$ ' + element.total.toLocaleString("es-MX", { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
+    elements.push(item);
+  });
 
-
-    $('#datatable_list').DataTable().clear();
-    $('#datatable_list').DataTable().destroy();
-    $('#datatable_list').DataTable({
-        responsive: true,
-        language: {
-            url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
-        },
-        data: elements,
-        columns: [
-            {title: "Membresía"},
-            {title: "Activos"},
-            {title: "Total"},
-
-        ],
-        paging: false,
-        searching: false,
-        info: false,
-        lengthChange: false,
-    });
+  // Limpiar y recrear la tabla
+  $('#datatable_list').DataTable().clear();
+  $('#datatable_list').DataTable().destroy();
+  $('#datatable_list').DataTable({
+    responsive: true,
+    language: {
+      url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json',
+    },
+    data: elements,
+    columns: [
+      {title: "Membresía"},
+      {title: "Activos"},
+      {title: "Total"},
+    ],
+    paging: false,
+    searching: false,
+    info: false,
+    lengthChange: false,
+  });
 }
 
 function update_graph(memberships) {
-    if (window.miGrafica) {
-        window.miGrafica.destroy();
+  if (window.miGrafica) {
+    window.miGrafica.destroy();
+  }
+
+  let data = {};
+
+  // Crear los datos para la gráfica
+  for (const membership of memberships) {
+    data[membership.name] = membership.total;
+  }
+
+  const labels = Object.keys(data);
+  const values = Object.values(data);
+
+  const ctx = document.getElementById("grafica").getContext("2d");
+
+  // Crear o actualizar la gráfica
+  window.miGrafica = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: labels,
+      datasets: [{
+        label: "Total por membresía",
+        data: values,
+        backgroundColor: ["#e56300", "#000000", "#FFCE56", "#ffda02", "#9C27B0"],
+      }]
+    },
+    options: {
+      responsive: false,
+      maintainAspectRatio: false
     }
-
-    let data = {}
-
-    for (const membership of memberships) {
-        data[membership.name]= membership.total;
-    }
-
-    const labels = Object.keys(data);
-    const values = Object.values(data);
-
-    const ctx = document.getElementById("grafica").getContext("2d");
-
-    window.miGrafica = new Chart(ctx, {
-        type: "pie",
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "Total por membresía",
-                data: values,
-                backgroundColor: ["#e56300", "#000000", "#FFCE56", "#ffda02", "#9C27B0"],
-            }]
-        },
-        options: {
-            responsive: false,
-            maintainAspectRatio: false
-        }
-    });
+  });
 }
-
